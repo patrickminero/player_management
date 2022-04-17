94335f4fdcd128dfea4d0f1483f42693f3fc8b1a
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-17 22:58:59 +0200

    clean up player routes

--- player/lib/routes/player_routes.rb
+++ player/lib/routes/player_routes.rb
@@ -1,23 +1,34 @@
+before '/players/:id' do
+  @player = Player.find(params[:id]) || {}
+end
+
 get '/teams/:team_id/players' do |team_id|
   Player.where(team_id: team_id).to_json
 end
 
-get '/players/:id' do |id|
-  Player.find(id).to_json
+get '/players/:id' do
+  return { status: 404 }.to_json unless @player.present?
+  @player.to_json
 end
 
-post '/teams/:team_id/players' do |team_id|
-  Player.create!(JSON.parse params[:player], team_id: team_id)
+post '/teams/:team_id/players' do
+  player = Player.new(ParamSanitizer.new(Player).sanitize(params[:player]))
+  player.team_id = params[:team_id]
+  if player.save!
+    player.to_json
+  else
+    {status: 404}
+  end
 end
 
 patch '/players/:id' do |id|
-  player = Player.find(id)
-  player.update(JSON.parse(params[:player]))
-  player
+  return { status: 404 }.to_json unless @player.present?
+  @player.update(ParamSanitizer.new(Player).sanitize(params[:player]))
+  @player.to_json
 end
 
 delete '/players/:id' do |id|
-  player = Player.find(id)
-  player.destroy
+  return { status: 404 }.to_json unless @player.present?
+  @player.destroy
   {status: 204}.to_json
 end
