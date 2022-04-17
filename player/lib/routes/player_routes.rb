ed691a0905c34061917e0c7b1373d297f6b772d4
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-17 23:11:15 +0200

    added message and correct error handling

--- player/lib/routes/player_routes.rb
+++ player/lib/routes/player_routes.rb
@@ -7,28 +7,28 @@ get '/teams/:team_id/players' do |team_id|
 end
 
 get '/players/:id' do
-  return { status: 404 }.to_json unless @player.present?
+  return { status: 404, error: 'player not found' }.to_json unless @player.present?
   @player.to_json
 end
 
 post '/teams/:team_id/players' do
   player = Player.new(ParamSanitizer.new(Player).sanitize(params[:player]))
   player.team_id = params[:team_id]
-  if player.save!
+  if player.save
     player.to_json
   else
-    {status: 404}
+    player.errors.to_json
   end
 end
 
 patch '/players/:id' do |id|
-  return { status: 404 }.to_json unless @player.present?
+  return { status: 404, error: 'player not found' }.to_json unless @player.present?
   @player.update(ParamSanitizer.new(Player).sanitize(params[:player]))
   @player.to_json
 end
 
 delete '/players/:id' do |id|
-  return { status: 404 }.to_json unless @player.present?
+  return { status: 404, error: 'player not found' }.to_json unless @player.present?
   @player.destroy
   {status: 204}.to_json
 end
