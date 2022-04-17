5552febfdc16ac6ad1fd4b0be09c107d98050026
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-17 18:07:52 +0200

    added patch and delete routes

--- lib/routes/team_routes.rb
+++ lib/routes/team_routes.rb
@@ -2,14 +2,26 @@ get '/teams' do
   Team.all.to_json
 end
 
-post '/teams' do
-  post = Team.create!(name: params[:name])
-  post.to_json
-end
-
-get '/teams/:team_id' do |team_id|
-  team = Team.find(team_id)
+get '/teams/:id' do |id|
+  team = Team.find(id)
   team.attributes.merge(
     players: team.players,
   ).to_json
+end
+
+post '/teams' do
+  team = Team.create!(name: params[:name])
+  team.to_json
+end
+
+patch '/teams/:id' do |id|
+  team = Team.find(id)
+  team.update(name: params[:name])
+  team.to_json
+end
+
+delete '/teams/:id' do |id|
+  team = Team.find(id)
+  team.destroy
+  { status: 204 }.to_json
 end
