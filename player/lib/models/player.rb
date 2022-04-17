f547effa0c58693e7cd302e1a38b1b91d667376c
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-17 22:55:49 +0200

    added team_id field to player

--- player/lib/models/player.rb
+++ player/lib/models/player.rb
@@ -4,7 +4,7 @@ class Player
   field :name, type: String
   field :position, type: String
   field :price, type: Integer
-
+  field :team_id, type: String
 
   validates :position, inclusion: { in: [ "goalkeeper", "defender", "midfield", "forward" ] }
 end
