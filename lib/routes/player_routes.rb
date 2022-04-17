30aa743e7d268e92910e4fa04d1a2530d7f081c0
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-17 18:24:36 +0200

    Added get method to single player

--- lib/routes/player_routes.rb
+++ lib/routes/player_routes.rb
@@ -2,6 +2,10 @@ get '/teams/:team_id/players' do |team_id|
   Team.find(team_id).players.to_json
 end
 
+get '/players/:id' do |id|
+  Player.find(id).to_json
+end
+
 post '/teams/:team_id/players' do |team_id|
   player = JSON.parse params[:player]
   team = Team.find(team_id)
