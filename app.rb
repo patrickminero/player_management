b37cef3ea8dd406fe6cc3c2c456efc887b0ef5cb
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-17 17:51:53 +0200

    added require all gem

--- app.rb
+++ app.rb
@@ -1,27 +1,6 @@
 require 'sinatra'
 require 'mongoid'
+require 'require_all'
+require_all 'lib'
 
 Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
-
-get '/teams' do
-  Team.all.to_json
-end
-
-post '/teams' do
-  post = Team.create!(name: params[:name])
-  post.to_json
-end
-
-get '/teams/:team_id' do |team_id|
-  team = Team.find(team_id)
-  team.attributes.merge(
-    players: team.players,
-  ).to_json
-end
-
-post '/teams/:team_id/players' do |team_id|
-  player = JSON.parse params[:player]
-  team = Team.find(team_id)
-  comment = team.players.create!(player)
-  team.attributes.merge(players: team.players).to_json
-end
