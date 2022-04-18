597d61e2a89e538de1de0e17698d3c478912d891
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-18 12:16:10 +0200

    clean up team routes

--- team/lib/routes/team_routes.rb
+++ team/lib/routes/team_routes.rb
@@ -1,24 +1,26 @@
+before '/teams/:id' do
+  @team = Team.find(params[:id]) || {}
+  halt 404, { message: 'Team not found' }.to_json unless @team.present?
+end
+
 get '/teams' do
   Team.all.to_json
 end
 
 get '/teams/:id' do |id|
-  Team.find(id).to_json
+  @team.to_json
 end
 
 post '/teams' do
-  team = Team.create!(name: params[:name])
-  team.to_json
+  team = Team.new(name: params[:name])
+  team.save ? team.to_json : team.errors.to_json
 end
 
 patch '/teams/:id' do |id|
-  team = Team.find(id)
-  team.update(name: params[:name])
-  team.to_json
+  @team.update(name: params[:name]) ? @team.to_json : @team.errors.to_json
 end
 
 delete '/teams/:id' do |id|
-  team = Team.find(id)
-  team.destroy
+  @team.destroy
   { status: 204 }.to_json
 end
