ceeb7f40c8b593262e7e9a7406586249d8494e7d
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-18 14:47:45 +0200

    added player factory

---
+++ player/spec/factories/player.rb
@@ -0,0 +1,11 @@
+require 'faker'
+require 'factory_bot'
+
+FactoryBot.define do
+  factory :player do
+    name { Faker::Sports::Football.player }
+    position { Faker::Sports::Football.position }
+    price { Faker::Number.number(digits: 7) }
+    team_id { Faker::Alphanumeric.alphanumeric(number: 10) }
+  end
+end
