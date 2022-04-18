08c56a8eb0292908cd00815e712c517fc82725a2
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-18 16:08:48 +0200

    change position value for player factory

--- player/spec/factories/player.rb
+++ player/spec/factories/player.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :player do
     name { Faker::Sports::Football.player }
-    position { Faker::Sports::Football.position }
+    position { [ "goalkeeper", "defender", "midfield", "forward" ].sample }
     price { Faker::Number.number(digits: 7) }
     team_id { Faker::Alphanumeric.alphanumeric(number: 10) }
   end
