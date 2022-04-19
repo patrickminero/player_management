c542f44f5325f25e5b5c327dc3401e9622e47a34
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-19 18:59:24 +0200

    adjusted player routes to use sequel syntax

--- player/lib/routes/player_routes.rb
+++ player/lib/routes/player_routes.rb
@@ -1,21 +1,21 @@
 before '/players/:id' do
-  @player = Player.find(params[:id]) || {}
-  halt 404, { message: 'Player not found' }.to_json unless @player.present?
+  @player = Player[params[:id].to_i]
+  halt 404, { message: 'Player not found' }.to_json if @player.nil?
 end
 
 get '/teams/:team_id/players' do |team_id|
-  Player.where(team_id: team_id).to_json
+  Player.where(team_id: team_id).map{|player| player.values}.to_json
 end
 
 get '/players/:id' do
-  @player.to_json
+  @player.values.to_json
 end
 
 post '/teams/:team_id/players' do
   PlayerBuilder.new(params).build
 end
 
-patch '/players/:id' do |id|
+patch '/players/:id' do
   PlayerUpdater.new(@player).update(params)
 end
 
