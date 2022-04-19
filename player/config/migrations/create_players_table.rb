a616470f4444c6d11431c0f1d6d01de3a3c8d167
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-19 18:58:04 +0200

    changed data type on players table

--- player/config/migrations/create_players_table.rb
+++ player/config/migrations/create_players_table.rb
@@ -4,7 +4,7 @@ DB = Sequel.connect('sqlite://players.db')
 DB.create_table :players do
   primary_key :id
   String :name
-  Integer :position
+  String :position
   Integer :price
   Integer :team_id
 end
