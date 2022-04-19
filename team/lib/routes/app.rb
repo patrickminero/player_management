4f1dc28663fa149b6faaa1b4e4314aa954a40425
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-19 13:39:29 +0200

    modified routes to use sequel instead of mongoid

--- team/lib/routes/app.rb
+++ team/lib/routes/app.rb
@@ -1,23 +1,27 @@
 before '/teams/:id' do
-  @team = Team.find(params[:id]) || {}
-  halt 404, { message: 'Team not found' }.to_json unless @team.present?
+  @team = Team[params[:id]] || {}
+  halt 404, { message: 'Team not found' }.to_json unless @team.exists?
 end
 
 get '/teams' do
-  Team.all.to_json
+  Team.all.map{|t| t.values}.to_json
 end
 
 get '/teams/:id' do |id|
-  @team.to_json
+  @team.values.to_json
 end
 
 post '/teams' do
   team = Team.new(name: params[:name])
-  team.save ? team.to_json : team.errors.to_json
+  halt 400, { message: team.errors }.to_json unless team.valid?
+  team.save
+  status 201
 end
 
 patch '/teams/:id' do |id|
-  @team.update(name: params[:name]) ? @team.to_json : @team.errors.to_json
+  halt 400, { message: "Name can't be blank" }.to_json if params[:name].empty?
+  @team.update(name: params[:name])
+  status 202
 end
 
 delete '/teams/:id' do |id|
