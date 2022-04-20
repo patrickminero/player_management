afd4e15ed1c00428a4aece37eeb2efa834c33389
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-20 10:13:35 +0200

    updated player factory values

--- player/spec/factories/player.rb
+++ player/spec/factories/player.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :player do
     name { Faker::Sports::Football.player }
-    position { [ "goalkeeper", "defender", "midfield", "forward" ].sample }
+    position { [ "goalkeeper", "defender", "midfielder", "forwarder" ].sample }
     price { Faker::Number.number(digits: 7) }
     team_id { Faker::Alphanumeric.alphanumeric(number: 10) }
   end
