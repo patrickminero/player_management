61e4ef2046aae0f2aaa9e6c5f9699dbe3d620d51
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-20 10:28:39 +0200

    changed logic to player spec

--- player/spec/lib/models/player_spec.rb
+++ player/spec/lib/models/player_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 RSpec.describe Player, type: :model do
   describe "validations" do
     it 'should validate the presence' do
-      expect(build(:player, name: '')).to be_invalid
-      expect(build(:player, price: nil)).to be_invalid
-      expect(build(:player, position: nil)).to be_invalid
-      expect(build(:player, team_id: nil)).to be_invalid
+      expect(build(:player, name: '')).not_to be_valid
+      expect(build(:player, price: nil)).not_to be_valid
+      expect(build(:player, position: nil)).not_to be_valid
+      expect(build(:player, team_id: nil)).not_to be_valid
     end
 
     it 'should not allow for a different position than the permitted ones' do
-      expect(build(:player, position: 'something')).to be_invalid
+      expect(build(:player, position: 'something')).not_to be_valid
     end
 
   end
