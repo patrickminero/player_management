9cce5ad1556dbba26dd03b930010ee636fbea6f8
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-20 10:29:44 +0200

    changed logic for player updater spec

--- player/spec/lib/models/player_updater_spec.rb
+++ player/spec/lib/models/player_updater_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 RSpec.describe PlayerUpdater, type: :model do
   describe 'PlayerUpdater' do
     context 'with valid params' do
-      let(:params) { {player: {name: 'John Smith', position: 'midfield', price: 30000}.to_json, team_id: '23asd132'} }
+      let(:params) { {player: {name: 'John Smith', position: 'goalkeeper', price: 30000}.to_json, team_id: '23asd132'} }
       let(:player) { create(:player) }
 
       let(:updated_player) { PlayerUpdater.new(player).update(params) }
 
       it 'should return a valid player in json format' do
-        expect(JSON.parse(updated_player)['name']).to eq 'John Smith'
+        expect(updated_player).to eq 202
       end
     end
 
@@ -20,7 +20,7 @@ RSpec.describe PlayerUpdater, type: :model do
       let(:updated_player) { PlayerUpdater.new(player).update(params) }
 
       it 'should return a hash with the error message' do
-        expect(updated_player).to eq ({position: ['is not included in the list']}.to_json)
+        expect(updated_player).to eq ({position: ['Enter a valid position']}.to_json)
       end
     end
   end
