246062e512c4b66106c14fc2f31bb98d2e691ada
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-20 18:34:43 +0200

    added player model spec

---
+++ query/spec/lib/models/player_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Player, type: :model do
+  describe "validations" do
+    it{ is_expected.to validate_presence :name }
+    it{ is_expected.to validate_presence :price }
+    it{ is_expected.to validate_presence :position }
+    it{ is_expected.to validate_presence :team_id }
+
+    it 'should not allow for a different position than the permitted ones' do
+      expect(build(:player, position: 'something')).not_to be_valid
+    end
+
+    describe 'associations' do
+      it{ is_expected.to have_many_to_one :team }
+    end
+  end
+end
