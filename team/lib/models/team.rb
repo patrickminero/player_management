cfe62ffb6b34f6334effdbc682d1b5b65c5c6cbd
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-21 17:53:20 +0200

    added lifecycle callbacks to team service

--- team/lib/models/team.rb
+++ team/lib/models/team.rb
@@ -5,14 +5,38 @@ class Team < Sequel::Model(:teams)
   end
 
   def after_create
-    conn = Bunny.new
-    conn.start
+    connection = Bunny.new
+    connection.start
 
-    ch = conn.create_channel
-    q  = ch.queue("teams")
-    x  = ch.default_exchange.publish(self.to_hash.to_json, :routing_key => q.name)
+    channel = connection.create_channel
+    queue = channel.queue("create.teams")
+    channel.default_exchange.publish(self.to_hash.to_json, :routing_key => queue.name)
     puts "sent #{self.name} to query service"
-    conn.close
+    connection.close
+    super
+  end
+
+  def after_update
+    connection = Bunny.new
+    connection.start
+
+    channel = connection.create_channel
+    queue = channel.queue("update.teams")
+    channel.default_exchange.publish(self.to_hash.to_json, :routing_key => queue.name)
+    puts "sent #{self.name} to query service"
+    connection.close
+    super
+  end
+
+  def after_destroy
+    connection = Bunny.new
+    connection.start
+
+    channel = connection.create_channel
+    queue = channel.queue("destroy.teams")
+    channel.default_exchange.publish(self.id.to_json, :routing_key => queue.name)
+    puts "sent #{self.name} to query service for destroy"
+    connection.close
     super
   end
 end
