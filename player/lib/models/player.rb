9e38dd30b616153dfa314cd1544f1585e79fb3f8
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-21 13:27:41 +0200

    added after create callback for player and team services

--- player/lib/models/player.rb
+++ player/lib/models/player.rb
@@ -5,4 +5,16 @@ class Player < Sequel::Model(:players)
     validates_presence [:name, :price, :team_id], message: 'Please fill all required fields', on: :create
     validates_includes ['goalkeeper', 'defender', 'midfielder', 'forwarder'], :position, message: 'Enter a valid position', on: :create
   end
+
+  def after_create
+    conn = Bunny.new
+    conn.start
+
+    ch = conn.create_channel
+    q  = ch.queue("player")
+    x  = ch.default_exchange.publish(self.to_hash.to_json, :routing_key => q.name)
+    puts "sent #{self.name} to query service"
+    conn.close
+    super
+  end
 end
--- team/lib/models/team.rb
+++ team/lib/models/team.rb
@@ -3,4 +3,16 @@ class Team < Sequel::Model(:teams)
     super
     errors.add(:name, 'cannot be empty') if !name || name.empty?
   end
+
+  def after_create
+    conn = Bunny.new
+    conn.start
+
+    ch = conn.create_channel
+    q  = ch.queue("teams")
+    x  = ch.default_exchange.publish(self.to_hash.to_json, :routing_key => q.name)
+    puts "sent #{self.name} to query service"
+    conn.close
+    super
+  end
 end
