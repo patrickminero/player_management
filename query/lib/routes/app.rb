76e78860629c2aa1618538f53de48c6759b5ace5
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-21 13:29:15 +0200

    modified routes logic for query service

--- query/lib/routes/app.rb
+++ query/lib/routes/app.rb
@@ -4,21 +4,29 @@ before ['/teams/:id', '/teams/:team_id/*'] do
 end
 
 get '/teams' do
-  Team.eager(:players).all.map{|team| team.to_hash.merge({players: team.players.map{|player| player.to_hash}})}.to_json
+  Team.all.map{|team| team.to_hash.merge({players: Player.where(team_id: team.id).map{|player| player.to_hash}})}.to_json
 end
 
 get '/teams/:id' do
-  @team.to_hash.merge(players: @team.players.map{|player| player.to_hash}).to_json
+  @team.to_hash.merge(players: get_players_by_team).to_json
 end
 
 get '/teams/:team_id/players' do |team_id|
-  params[:position] ? @team.by_position(params[:position]).map{|player| player.to_hash}.to_json : @team.players.map{|player| player.to_hash}.to_json
+  params[:position] ?
+    Player.where(team_id: @team.id, position: params[:position]).map{|player| player.to_hash}.to_json : 
+    get_players_by_team.to_json
 end
 
 get '/players' do
-  params[:position] ? Player.by_position(params[:position]).map{|player| player.to_hash}.to_json : Player.all.map{|player| player.to_hash}.to_json
+  params[:position] ?
+    Player.by_position(params[:position]).map{|player| player.to_hash}.to_json :
+    Player.all.map{|player| player.to_hash}.to_json
 end
 
-get '/players/:id' do |position|
-  Player[:id].to_hash.to_json
+get '/players/:id' do |id|
+  Player[params[:id]].to_hash.to_json
+end
+
+def get_players_by_team
+  Player.where(team_id: @team.id).map{|player| player.to_hash}
 end
