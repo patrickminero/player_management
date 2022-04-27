5827d8ef1eefd130d7a0d4bf5a8de51e45e18d10
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-27 17:11:56 +0200

    fixed controller call to commands

--- app/controllers/players/players_controller.rb
+++ app/controllers/players/players_controller.rb
@@ -1,25 +1,25 @@
 class PlayersController < Sinatra::Base
   get '/players' do
-    App::Commands::IndexPlayer.call(params[:position]).to_json
+    App::Commands::Players::IndexPlayer.call(params[:position]).to_json
   end
 
   get '/players/:id' do
-    player = App::Commands::GetPlayer.call(params[:id]).to_hash.to_json
-    player.errors.empty? ? 200 : player.errors.to_hash.to_json
+    player = App::Commands::Players::GetPlayer.call(params[:id])
+    player ? player.to_hash.to_json : 404
   end
 
   post '/teams/:team_id/players' do
-    player = App::Commands::CreatePlayer.call(params)
+    player = App::Commands::Players::CreatePlayer.call(params)
     player.errors.empty? ? 201 : player.errors.to_hash.to_json
   end
 
   patch '/players/:id' do
-    player = App::Commands::UpdatePlayer.call(params)
+    player = App::Commands::Players::UpdatePlayer.call(params)
     player.errors.empty? ? 202 : player.errors.to_hash.to_json
   end
 
   delete '/players/:id' do |id|
-    player = App::Commands::DeletePlayer.call(params[:id])
+    player = App::Commands::Players::DeletePlayer.call(params[:id])
     player ? 204 : 404
   end
 end
