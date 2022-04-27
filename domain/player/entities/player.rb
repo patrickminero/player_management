add4efd54f9eca9c83793da0a2db1f9cc5cbd4e8
Patrick Minero <patrickminerovalencia@gmail.com>
2022-04-27 17:59:37 +0200

    fixed indentation and EOL

--- domain/player/entities/player.rb
+++ domain/player/entities/player.rb
@@ -5,6 +5,7 @@ module Domain
         attr_reader :name, :price, :position, :team_id
 
         def initialize params
+          @id = params[:id] if params[:id]
           @name = params[:name]
           @price = params[:price]
           @position = params[:position]
@@ -12,7 +13,9 @@ module Domain
         end
 
         def to_hash
-          {name: @name, price: @price, position: @position, team_id: @team_id.to_i}
+          @id ? 
+            {id: @id, name: @name, price: @price, position: @position, team_id: @team_id.to_i} :
+            {name: @name, price: @price, position: @position, team_id: @team_id.to_i}
         end
       end
     end
